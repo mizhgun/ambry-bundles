40749a86714ff45852443cfc7f8a6e231c8b406d
MiZHGUN <mizhgun@gmail.com>
2015-07-15 01:12:39 +0300

    Fixed descriptions

--- medicare.gov/compare-hospitals/bundle.py
+++ medicare.gov/compare-hospitals/bundle.py
@@ -1,5 +1,6 @@
 from ambry.bundle.loader import CsvBundle
 
 class Bundle(CsvBundle):
-    def line_mangler(self, source, row_gen, l):
-        return l.replace('\0', '')
+    # def line_mangler(self, source, row_gen, l):
+    #     return l.replace('\0', '')
+    pass
